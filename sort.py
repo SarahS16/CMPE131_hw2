4114265d68b619d6ab87ed25fe12ef30122a5d7a
Sarah Singh <sarah.singh@gmail.com>
2022-03-08 18:29:26 -0800

    fixing sort.py again

--- sort.py
+++ sort.py
@@ -3,7 +3,7 @@ def sort_list(list):
     i = 0
     while i < (k):
         j = 0
-        while 0 < j < (k-1):
+        while j < (k-1):
             if list[j] > list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
             j += 1
