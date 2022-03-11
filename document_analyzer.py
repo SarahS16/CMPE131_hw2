3a0af101e28eb7f4fa4e27ff373b96d9f6fafdac
Sarah Singh <sarah.singh@gmail.com>
2022-03-10 23:58:09 -0800

    finalizing analyzer

--- document_analyzer.py
+++ document_analyzer.py
@@ -12,10 +12,10 @@ for word in words:
 
 sorted_val = dict(sorted(counter.items(), key = lambda x:x[1], reverse = True))
 
-print('\r')
+print()
 for keys in list(sorted_val.keys())[0:5]:
     print(keys, ":", sorted_val[keys])
 
-print('\r')
+
 
 
